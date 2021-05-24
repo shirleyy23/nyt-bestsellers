fc8fd902165f2e4cfdaeb38f4a8f2e1dcdbc986c
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2021-05-24 14:24:58 -0400

    added core components

---
+++ src/app/modules/core/components/link/link.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, Output, EventEmitter, ViewEncapsulation} from '@angular/core';
+import { LinkType, Buttons, Links } from 'Core/models/frontend/frontend-constants';
+
+
+@Component({
+	selector: 'NavLink',
+	templateUrl: './link.component.html',
+	styleUrls: ['./link.component.scss'],
+	encapsulation: ViewEncapsulation.Emulated
+})
+
+export class Link {
+	constants = {
+		type: {
+			inline: Links.inline,
+			primary: Buttons.primary,
+			secondary: Buttons.secondary
+		}
+	}
+	@Input() public text: string = '';
+	@Input() public type: LinkType = this.constants.type.inline
+	@Input() public url: string = '';
+	@Output() clickEvent = new EventEmitter<any>();
+	constructor() {};
+
+	public onClick() {
+		!!this.clickEvent && this.clickEvent.emit()
+	}
+}
