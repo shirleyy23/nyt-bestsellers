96836fa57edb83b31fb924f7c014b1cea595e1e6
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2021-05-24 14:26:46 -0400

    updated front end constants

---
+++ src/app/modules/core/models/frontend/frontend-constants.ts
@@ -0,0 +1,19 @@
+export enum Buttons {
+	primary,
+	secondary,
+}
+
+export enum Links {
+	inline,
+}
+
+export type LinkType = Buttons | Links;
+
+export enum Vendors {
+	amazon = 'Amazon',
+	appleBooks = 'Apple Books',
+	barnesAndNoble = 'Barnes and Noble',
+	booksAMillion = 'Books-A-Million',
+	bookShop = 'Bookshop',
+	indieBound = 'IndieBound'
+}
